fffda5e4099e4416cd1e4ca8cc672fc64aac18b3
RICHARD GRIFFITHS <griff182uk@yahoo.co.uk>
2022-12-04 08:39:31 +0000

    done useffect

--- app/dojo-blog/src/Home.js
+++ app/dojo-blog/src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
@@ -9,9 +9,23 @@ const Home = () => {
             { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
         ]
     );
+
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id != id);
+        setBlogs(newBlogs);
+    }
+
+    const [name, setName] = useState('mario');
+
+    useEffect(() => {
+        console.log('use effect run');
+        console.log(name);
+    }, [name]);
+
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="All blogs"/>
+            <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete} />
+            <button onClick={() => setName('luigi')}>change name</button>
         </div>
     );
 }
